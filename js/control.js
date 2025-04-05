f8d3467b60f65847d41c9e1c60eea2eaf401684a
phamnu304 <phamthitrinhnu304@gmail.com>
2025-04-05 14:16:03 +0700

    Add files via upload

---
+++ js/control.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+var typed = new Typed('.element', {
+               	strings: ["Web Designer.", "Code Developer.","Apps Designer.", "2D Artist."],
+       	 	typeSpeed:50,
+        		backSpeed:50,
+        		loop:true,
+        		});
+$('.counter').countUp();
+
+AOS.init();
+
